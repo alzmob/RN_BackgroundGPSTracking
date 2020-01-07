43f4fb6ede9bc17b1e70fcd40217874288507caf
alzmob <top.girl.love@yandex.com>
2020-01-07 01:28:49 -0800

    Update background mode.

--- App.js
+++ App.js
@@ -1,41 +1,87 @@
 import React from 'react';
 import {
   SafeAreaView,
-  View,
   Text,
   TouchableOpacity,
 } from 'react-native';
-import Geolocation from '@react-native-community/geolocation';
-import BackgroundTimer from 'react-native-background-timer';
+import { Alert } from 'react-native';
+import BackgroundGeolocation from '@mauron85/react-native-background-geolocation';
+import moment from 'moment'
 
 
 export default class App extends React.Component {
-
-  componentWillMount() {
-    BackgroundTimer.runBackgroundTimer(() => { 
-      Geolocation.getCurrentPosition((position) => {
-        console.log('latitude=======', position.coords.latitude)
-        console.log('longitude=======', position.coords.longitude)
-
-        var url='https://lameen.free.beeceptor.com/rntest'
-        fetch(url, {
-            method: 'POST',
-            body: JSON.stringify({
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude
-            })
-        })
-        .then((response) => response.json())
-        .then((responseData) => {
-            console.log('Notificatins======', responseData)
-        }).catch((error) => {
-          console.error(error);
-        });
-
-      });
-    }, 
-    1000*60*15);
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 37.33680278,
+      longitude: 122.08362627,
+      lastDate: new Date()
+    }
   }
+
+
+  componentDidMount() {
+    BackgroundGeolocation.configure({
+      desiredAccuracy: BackgroundGeolocation.HIGH_ACCURACY,
+      stationaryRadius: 1,
+      distanceFilter: 1,
+      notificationTitle: 'Background tracking',
+      notificationText: 'enabled',
+      debug: false,
+      startOnBoot: false,
+      stopOnTerminate: true,
+      locationProvider: BackgroundGeolocation.ACTIVITY_PROVIDER,
+      interval: 10000,
+      fastestInterval: 5000,
+      activitiesInterval: 10000,
+      stopOnStillActivity: false,
+    });
+
+    BackgroundGeolocation.on('stationary', (stationaryLocation) => {
+      
+    });
+
+    BackgroundGeolocation.on('location', (location) => {
+      BackgroundGeolocation.startTask(taskKey => {
+        var currentDate = new Date()
+        var duration = (currentDate - this.state.lastDate) / 1000
+        if (duration > 5) {
+          console.log('Location ============',location.latitude,  location.longitude)
+          this.setState({lastDate: new Date(), latitude:location.latitude, longitude:location.longitude})
+          this.callServer()
+        }
+        BackgroundGeolocation.endTask(taskKey);
+      });
+    });
+
+    BackgroundGeolocation.on('authorization', (status) => {
+      if (status !== BackgroundGeolocation.AUTHORIZED) {
+        setTimeout(() =>
+          Alert.alert('App requires location tracking permission', 'Would you like to open app settings?', [
+            { text: 'Yes', onPress: () => BackgroundGeolocation.showAppSettings() },
+            { text: 'No', onPress: () => console.log('No Pressed'), style: 'cancel' }
+          ]), 1000);
+      }
+    });
+
+    BackgroundGeolocation.checkStatus(status => {
+      if (!status.isRunning) {
+        BackgroundGeolocation.start();
+      }
+    });
+  }
+
+
+  componentWillUnmount() {
+    BackgroundGeolocation.removeAllListeners();
+  }
+
+  callServer() {
+    console.log('Server Call with this.state !!!!!!!!!! ')
+    // you can call server here.
+  }
+
+
 
   render () {
     return (
